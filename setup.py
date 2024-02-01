2aa6814e1b350553482ec421738478d18cdbc2a2
LucaDeGrandis <luca.degrandis@altiliagroup.com>
2024-02-01 15:25:41 +0100

    created main structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name="drpe",
+    version="0.1.0",
+    description="Diverse Role-Player for Summarization Generation Evaluation",
+    author="Luca De Grandis",
+    author_email="luca.degrandis@altiliagroup.com",
+)
