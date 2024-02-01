8b7384d3324669c834b5f61c91f4f7f47375bf78
LucaDeGrandis <luca.degrandis@altiliagroup.com>
2024-02-01 17:05:28 +0100

    various changes

--- run.py
+++ run.py
@@ -46,12 +46,12 @@ def dynamic_roles_generator(
     # Generate coarse grained roles
     coarse_grained_prompt = PromptTemplate.from_template(DYNAMIC_TEMPLATES['coarse_grained'])
     chain = LLMChain(llm=model, prompt=coarse_grained_prompt)
-    coarse_grained_roles = chain.invoke(text=input_text)['text']
+    coarse_grained_roles = chain.invoke(input=input_text)['text']
 
     # Generate fine grained roles
     fine_grained_prompt = PromptTemplate.from_template(DYNAMIC_TEMPLATES['fine_grained'])
     chain = LLMChain(llm=model, prompt=fine_grained_prompt)
-    fine_grained_roles = chain.invoke(text=input_text)['text']
+    fine_grained_roles = chain.invoke(input=input_text)['text']
 
     return coarse_grained_roles, fine_grained_roles
 
@@ -62,7 +62,7 @@ def argparser():
     parser.add_argument('out_file', type=str, help='The output path. It must be a jsonl file.')
     parser.add_argument('openai_key', type=str, help='The OpenAI key.')
     parser.add_argument('--verbose', action='store_true', help='Saves intermediate results.')
-    parser.add_argument('--roles_generator', type=str, default='gpt-3.5-turbo-1106', help='THe model used to generate the dynamic roles.')
+    parser.add_argument('--roles_generator', type=str, default='gpt-3.5-turbo-instruct', help='THe model used to generate the dynamic roles.')
     parser.add_argument('--roles_generator_templates', type=int, default=32, help='THe model used to generate the dynamic roles.')
     return parser.parse_args()
 
