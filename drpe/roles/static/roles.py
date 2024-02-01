e6d286e8683a7bac4e90dde6848f620d9abacdc3
LucaDeGrandis <luca.degrandis@altiliagroup.com>
2024-02-01 15:29:28 +0100

    added static roles

---
+++ drpe/roles/static/roles.py
@@ -0,0 +1,8 @@
+# Static roles for DRPE
+
+
+STATIC_ROLES = {
+    'general_public': ' These readers may be interested in the story and looking for updates on the investigation',
+    'critic': 'These people will check fluent writing, clear sentences, and good wording in summary writing.',
+    'news_author': 'These readers will check the summary for consistency with the original article.',
+}
