b228499615c2c260e534f1ba219539b702da75a7
LucaDeGrandis <luca.degrandis@altiliagroup.com>
2024-02-02 09:37:18 +0100

    minor correction to static roles

--- drpe/roles/static/roles.py
+++ drpe/roles/static/roles.py
@@ -2,7 +2,7 @@
 
 
 STATIC_ROLES = [
-    ['General Public', 'These readers may be interested in the story and looking for updates on the investigation.'],
-    ['Critic', 'These people will check fluent writing, clear sentences, and good wording in summary writing.'],
-    ['News Author', 'These readers will check the summary for consistency with the original article.'],
+    'General Public: These readers may be interested in the story and looking for updates on the investigation.',
+    'Critic: These people will check fluent writing, clear sentences, and good wording in summary writing.',
+    'News Author: These readers will check the summary for consistency with the original article.',
 ]
